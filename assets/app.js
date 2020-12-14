9882fab3c77f887d5f981164016c21c7b1cc729d
Th G <thida.dev@gmail.com>
2020-12-14 12:29:46 +0100

    MàJ 14-12-2020 12h30

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,15 @@
-const boutonsRech = document.querySelectorAll('button');
+// blocage rechargement de la page lors d'un clic sur un bouton
+const boutonsRech = document.querySelectorAll('#buttons button');
 console.log(boutonsRech);
 boutonsRech.forEach((boutons) => {
     boutons.addEventListener('click', (event) => {
         event.preventDefault();
     })
 });
+
+
+// Activation des tooltips Bootstrap sur boutons de recherche
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
