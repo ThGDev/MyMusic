a4515d11b0ab38b2e1f3cd72de8c1a3460e2ac1b
Th G <thida.dev@gmail.com>
2020-12-15 20:12:07 +0100

    MàJ 15-12-2020 20h10

--- assets/search.js
+++ assets/search.js
@@ -11,13 +11,16 @@
 ----------------------------------- */
 
 const boutonDeezer = document.querySelector('.btn-dz');
+const resultatsRecherche = document.querySelector('#resultRecherche');
 const resultatsRechercheArtistes = document.querySelector('#resultRechercheArtiste');
 const resultatsRechercheTitres = document.querySelector('#resultRechercheTitre');
 const artistes = document.querySelector('#artistes');
+const inputArtiste = document.querySelector('#inputArtiste');
+const inputTitre = document.querySelector('#inputTitre');
+const errorMsg = document.querySelector('#errorMsg');
 const nomArtiste = 'AC/DC';
 const cheminImagesArtistes = 'img/zik/search/artiste/';
 const cheminImagesTitres = 'img/zik/search/titre/';
-const nbreArtistesAffiches = 12;
 
 // ---------------------------- POUR LES ARTISTES
 const imagesArtistes = [`${cheminImagesArtistes}0.jpg`, `${cheminImagesArtistes}1.jpg`, `${cheminImagesArtistes}2.jpg`, `${cheminImagesArtistes}3.jpg`, `${cheminImagesArtistes}4.jpg`, `${cheminImagesArtistes}5.jpg`, `${cheminImagesArtistes}6.jpg`, `${cheminImagesArtistes}7.jpg`, `${cheminImagesArtistes}8.jpg`, `${cheminImagesArtistes}9.jpg`, `${cheminImagesArtistes}10.jpg`, `${cheminImagesArtistes}11.jpg`, ];
@@ -28,7 +31,7 @@ const remplissageArtistes = () => {
         resultatsRechercheArtistes.insertAdjacentHTML('beforeend',
         `<div class="blocRechArtiste col">
             <img class="img-fluid rounded" src="${image}" alt="Image-Artiste">
-            <a href="#"><p class="badge rounded-pill bg-secondary">${nomArtiste}</p></a>
+            <a href="#"><p class="badge rounded-pill bg-secondary">Titre Album</p></a>
         </div>`
         )
     })
@@ -60,4 +63,37 @@ const affichageRechercheTitres = () => {
     titres.style.display = 'block';
 }
 
-// boutonDeezer.addEventListener('click', affichageRechercheArtistes);
+// ---------------------------- FONCTION DE TEST DE CHAMPS
+//* au clic sur le bouton DEEZER :
+//* si le champs "artiste" est rempli, alors j'affiche les résultats des artistes
+//* si le champs "titre" est rempli, alors j'affiche les résultats des titres
+//* si les 2 champs sont vides OU si les 2 champs sont remplis → MSG ERREUR
+
+const verifChampsEtAffichage = () => {
+    
+    if (inputArtiste.value === '' && inputTitre.value === ''){
+        artistes.style.display = 'none';
+        titres.style.display = 'none';
+        errorMsg.innerHTML = '<p class="alert alert-danger" role="alert">Veuillez saisir quelque chose svp !</p>';
+    }
+    if (inputArtiste.value !== '' && inputTitre.value !== ''){
+        artistes.style.display = 'none';
+        titres.style.display = 'none';
+        errorMsg.innerHTML = '<p class="alert alert-danger" role="alert">Veuillez ne remplir qu\'un seul champs svp !</p>';
+    }
+    if(inputArtiste.value !== '' && inputTitre.value === ''){
+        errorMsg.innerHTML = '';
+        titres.style.display = 'none';
+        affichageRechercheArtistes();
+    }
+    if(inputArtiste.value === '' && inputTitre.value !== ''){
+        errorMsg.innerHTML = '';
+        artistes.style.display = 'none';
+        affichageRechercheTitres();
+    }
+    // else{
+    //     errorMsg.innerHTML = '';
+    // }
+}
+
+boutonDeezer.addEventListener('click', verifChampsEtAffichage);
