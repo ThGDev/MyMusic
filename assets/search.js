4badd4ea3a08d1e81a33d9ffa6d64c63ef361bc1
Th G <thida.dev@gmail.com>
2020-12-15 18:19:15 +0100

    MàJ 15-12-2020 18h20

--- assets/search.js
+++ assets/search.js
@@ -12,6 +12,7 @@
 
 const boutonDeezer = document.querySelector('.btn-dz');
 const resultatsRechercheArtistes = document.querySelector('#resultRechercheArtiste');
+const resultatsRechercheTitres = document.querySelector('#resultRechercheTitre');
 const artistes = document.querySelector('#artistes');
 const nomArtiste = 'AC/DC';
 const cheminImagesArtistes = 'img/zik/search/artiste/';
@@ -20,7 +21,7 @@ const nbreArtistesAffiches = 12;
 
 // ---------------------------- POUR LES ARTISTES
 const imagesArtistes = [`${cheminImagesArtistes}0.jpg`, `${cheminImagesArtistes}1.jpg`, `${cheminImagesArtistes}2.jpg`, `${cheminImagesArtistes}3.jpg`, `${cheminImagesArtistes}4.jpg`, `${cheminImagesArtistes}5.jpg`, `${cheminImagesArtistes}6.jpg`, `${cheminImagesArtistes}7.jpg`, `${cheminImagesArtistes}8.jpg`, `${cheminImagesArtistes}9.jpg`, `${cheminImagesArtistes}10.jpg`, `${cheminImagesArtistes}11.jpg`, ];
-// console.log(images);
+
 
 const remplissageArtistes = () => {
     imagesArtistes.forEach((image) => {
@@ -40,11 +41,11 @@ const affichageRechercheArtistes = () => {
 
 // ---------------------------- POUR LES TITRES
 const imagesTitres = [`${cheminImagesTitres}0.jpg`, `${cheminImagesTitres}1.jpg`, `${cheminImagesTitres}2.jpg`, `${cheminImagesTitres}3.jpg`, `${cheminImagesTitres}4.jpg`, `${cheminImagesTitres}5.jpg`, `${cheminImagesTitres}6.jpg`, `${cheminImagesTitres}7.jpg`, `${cheminImagesTitres}8.jpg`, `${cheminImagesTitres}9.jpg`, `${cheminImagesTitres}10.jpg`, `${cheminImagesTitres}11.jpg`, ];
-console.log(images);
+
 
 const remplissageTitres = () => {
     imagesTitres.forEach((image) => {
-        resultatsRechercheArtistes.insertAdjacentHTML('beforeend',
+        resultatsRechercheTitres.insertAdjacentHTML('beforeend',
         `<div class="blocRechTitre col">
             <img class="img-fluid rounded" src="${image}" alt="Image-Titre">
             <a href="#"><span class="badge rounded-pill bg-light text-dark">Artiste</span></a>
